760596366a704dec093d9a55cabaac373cdde230
Michael Manning <michaelmanning@gmail.com>
2016-09-19 16:38:14 -0600

    Updates

--- hw1/Server.py
+++ hw1/Server.py
@@ -30,62 +30,61 @@ class Server (BaseHTTPRequestHandler):
         htmlFile.close()
 
         # Render Html from template
-        html = template.render(board="Board Data")
+        html = template.render(self.board="board")
 
         # Format html string to utf8 and send
         self.wfile.write(bytes(html, "utf8"))
         return
 
-    def do_POST(x_cord, y_cord):
-        spot = check(x_cord, y_cord);
+    def do_POST(self):
+        content_length = int(self.headers.getHeader('content-length', 0))
+        content = self.rfile.read(content_length)
+        print content
+        return
+
+        # Spot is not on the board
+        if(x < 0 || x > 10 || y < 0 || y > 10)
+            SimpleHTTPRequestHandler.http_error(404)
+
+        spot = check(x_cord, y_cord)
         if(spot = "~"):
-            spot.set("X");
-            print("Miss!");
+            spot.set("X")
+            print("Miss!")
         else if(spot = "C"):
-            C -= 1;
-            spot.set("X");
-            print("Hit!");
-        else if(spot = "B"):
-            B -= 1;
-            spot.set("X");
-            print("Hit!");
-        else if(spot = "R"):
-            R -= 1;
-            spot.set("X");
+            C -= 1
+            spot.set("X")
             print("Hit!")
         else if(spot = "B"):
-            S -= 1;
-            spot.set("X");
-            print("Hit!");
+            B -= 1
+            spot.set("X")
+            print("Hit!")
         else if(spot = "R"):
-            D -= 1;
-            spot.set("X");
+            R -= 1
+            spot.set("X")
+            print("Hit!")
+        else if(spot = "B"):
+            S -= 1
+            spot.set("X")
+            print("Hit!")
+        else if(spot = "R"):
+            D -= 1
+            spot.set("X")
             print("Hit!")
         # If spot is already been guessed
         else if(spot = "X"):
-            SimpleHTTPRequestHandler.http_error_410();
+            SimpleHTTPRequestHandler.http_error(410)
             print("Already guessed this spot")
-        # Spot is not on the board
-        else:
-            SimpleHTTPRequestHandler.http_error_404();
-            print("Out of Bounds")
+
+
+
 
     def check(self, x, y):
         return self.board[x][y]
-
-    def print_board(self):
-        str
-        return str
 
 
 if __name__ == '__main__':
 
     if(sys.argv[1:]):
-        port = sys.argv[1]
-    else:
-        port = 5000
-
-    if(sys.argv[2:]):
         filename = sys.argv[2]
     else:
         filename = "board.txt"
@@ -100,7 +99,7 @@ if __name__ == '__main__':
     file.close()
 
     # Create server address tuple
-    address = ('', port)
+    address = ('128.111.52.245', 5000)
 
     # Pass in the server handler and the address
     httpd = HTTPServer(address, server)
