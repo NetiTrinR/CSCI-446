2f7e62355e9f59c91246c0f71d2f60c29ddfe0c1
Michael Manning <michaelmanning390@gmail.com>
2016-09-19 14:40:21 -0600

    Started on get method with html templating engine

--- hw1/Server.py
+++ hw1/Server.py
@@ -1,58 +1,82 @@
 #!/usr/bin/python3
 
-import sys;
-import HTTPServer;
+import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from jinja2 import Template
 
-class Server (SimpleHTTPRequestHandler):
+
+class Server (BaseHTTPRequestHandler):
 
     def __init__(self, file):
-        self.board = [[c for c in line[0:10]] for line in file];
-        self.C = 5;
-        self.B = 4;
-        self.R = 3;
-        self.S = 3;
-        self.D = 2;
-        self.protocol_version = "HTTP/1.1";
+        self.board = [[c for c in line[0:10]] for line in file]
+        self.C = 5
+        self.B = 4
+        self.R = 3
+        self.S = 3
+        self.D = 2
+        self.protocol_version = "HTTP/1.1"
 
-    def do_GET():
-        print("totes html");
+    def do_GET(self):
+        # Response Code
+        self.send_response(200)
 
-    def do_POST():
-        print("Something else");
+        # Headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-    def check(x, y):
-        return board[x][y];
+        # Create template from html
+        htmlFile = open("index.html", "r")
+        template = Template(htmlFile.read())
+        htmlFile.close()
+
+        # Render Html from template
+        html = template.render(board="Board Data")
+
+        # Format html string to utf8 and send
+        self.wfile.write(bytes(html, "utf8"))
+        return
+
+    def do_POST(self):
+        print("Something else")
+
+    def check(self, x, y):
+        return self.board[x][y]
+
+    def print_board(self):
+        str
+        return str
+
 
 if __name__ == '__main__':
 
     if(sys.argv[1:]):
-        port = argv[1]
+        port = sys.argv[1]
     else:
-        port = 5000;
+        port = 5000
 
     if(sys.argv[2:]):
-        filename = argv[2];
+        filename = sys.argv[2]
     else:
-        filename = "board.txt";
+        filename = "board.txt"
 
     # Open the file
-    file = open(filename, "r");
+    file = open(filename, "r")
 
     # Instanciate our server
-    server = Server(file);
+    server = Server(file)
 
     # Close the file
-    file.close();
+    file.close()
 
     # Create server address tuple
-    address = ('', port);
+    address = ('', port)
 
     # Pass in the server handler and the address
-    httpd = HTTPServer(address, server);
+    httpd = HTTPServer(address, server)
 
     # Lets say something
-    sa = httpd.socket.getsockname();
-    print("Serving HTTP on", sa[0], "port", sa[1], "...");
+    sa = httpd.socket.getsockname()
+    print("Serving HTTP on", sa[0], "port", sa[1], "...")
 
     # Serve it forever
-    httpd.serve_forever();
+    httpd.serve_forever()
